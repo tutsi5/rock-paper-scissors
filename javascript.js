6c83298cd89d41126713f22b29579c90d4f7d514
tutsi <tuta.matea@gmail.com>
2024-01-12 16:11:31 +0100

    Add one round

--- javascript.js
+++ javascript.js
@@ -10,3 +10,39 @@ function getComputerChoice() {
     }
 } 
 
+function playRound(playerSelection, computerSelection) {
+
+    let computer = computerSelection.toLowerCase();
+    let player = playerSelection.toLowerCase();
+
+    if (computer == "rock") {
+        if (player == "scissors") {
+        return "You Lose! Rock beats Scissors";
+        } else if (player == "paper") {
+            return "You Win! Paper beats Rock";
+        } else {
+            return "Tie! Play again!";
+        }
+    } else if (computer == "paper") {
+        if (player == "rock") {
+            return "You Lose! Paper beats Rock";
+        } else if (player == "scissors") {
+            return "You Win! Scissors beats Paper";
+        } else {
+            return "Tie! Play again!";        
+        }
+    } else {
+        if (player == "rock") {
+            return "You Win! Rock beats Scissors";
+        } else if (player == "paper") {
+            return "You Lose! Scissors beats Paper";
+        } else {
+            return "Tie! Play again!";
+        }
+    }
+}
+
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
